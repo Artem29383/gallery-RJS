858cb063a7488b7df4fdb3201a72cf6f87507531
Artem <artyom.aweryanow@yandex.ru>
2020-01-13 17:02:17 +0300

    Updates

---
+++ precache-manifest.8a77b93ea6022f521973e5743d606f9d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "411a242971e437c6487aa34bf1ed1d91",
+    "url": "/gallery-RJS/index.html"
+  },
+  {
+    "revision": "a990594703e23fd84cff",
+    "url": "/gallery-RJS/static/css/main.bd3cfff2.chunk.css"
+  },
+  {
+    "revision": "2fcc74a1b2d34b6a9246",
+    "url": "/gallery-RJS/static/js/2.5427a80b.chunk.js"
+  },
+  {
+    "revision": "8a14dcea7ad8b593c1e83c11f7ba1a89",
+    "url": "/gallery-RJS/static/js/2.5427a80b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a990594703e23fd84cff",
+    "url": "/gallery-RJS/static/js/main.70eebc0f.chunk.js"
+  },
+  {
+    "revision": "e987b6a6c4f5074809cd",
+    "url": "/gallery-RJS/static/js/runtime-main.945e2431.js"
+  },
+  {
+    "revision": "94a7f394ad6daa7b98b0870ef4084193",
+    "url": "/gallery-RJS/static/media/5.94a7f394.jpg"
+  },
+  {
+    "revision": "75fd0b9090247882c700e34fa8ed9c3e",
+    "url": "/gallery-RJS/static/media/HelveticaNeueCyr-Black.75fd0b90.otf"
+  },
+  {
+    "revision": "7ac1c8f0b974a943aa67859c07f86bd2",
+    "url": "/gallery-RJS/static/media/HelveticaNeueCyr-Bold.7ac1c8f0.otf"
+  },
+  {
+    "revision": "2127aa6d0680b144a3bd8be9debeba09",
+    "url": "/gallery-RJS/static/media/HelveticaNeueCyr-Light.2127aa6d.otf"
+  },
+  {
+    "revision": "c88edbac48d6408ef042ad663b7d9498",
+    "url": "/gallery-RJS/static/media/HelveticaNeueCyr-Medium.c88edbac.otf"
+  },
+  {
+    "revision": "e80de597659bb250f778c301c8cf380d",
+    "url": "/gallery-RJS/static/media/favicon.e80de597.ico"
+  },
+  {
+    "revision": "8348ecbef724b5e2addda6cc21d456dd",
+    "url": "/gallery-RJS/static/media/search.8348ecbe.svg"
+  },
+  {
+    "revision": "47f406f9a1b3cce57b07e71323140b29",
+    "url": "/gallery-RJS/static/media/search2.47f406f9.svg"
+  },
+  {
+    "revision": "a3d1312d2ad7c344f2dfa5bea3d64e10",
+    "url": "/gallery-RJS/static/media/standart_profile-user.a3d1312d.png"
+  }
+]);
